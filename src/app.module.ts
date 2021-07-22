0cfd0cf91ad67ca9be0d2db4d216dd49a7b8d495
SeungGumon <b9327912@gmail.com>
2021-07-23 00:55:43 +0900

    create user Module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { GraphQLModule } from '@nestjs/graphql';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot({
+      autoSchemaFile: true,
+    }),
+    UserModule,
+  ],
   providers: [AppService],
 })
 export class AppModule {}
