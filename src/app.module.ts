4efa2782b2c45504816a5eb3a792d279e1020f85
SeungGumon <b9327912@gmail.com>
2021-07-23 01:25:51 +0900

    download nest/configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,24 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
-    UserModule,
     GraphQLModule.forRoot({
       autoSchemaFile: true,
     }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'seunggumon',
+      password: '12345',
+      database: 'seokkao-page',
+      synchronize: true,
+      logging: false,
+    }),
+    UserModule,
   ],
   providers: [],
 })
