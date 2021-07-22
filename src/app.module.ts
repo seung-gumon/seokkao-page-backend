29c80d9bd81e7d45f9cb669f0c6e78457c8308e6
SeungGumon <b9327912@gmail.com>
2021-07-23 01:32:27 +0900

    add env and download crossEnv

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,14 @@ import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { GraphQLModule } from '@nestjs/graphql';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: process.env.NODE_ENV === 'dev' ? '.env.dev' : '.env.prod',
+    }),
     GraphQLModule.forRoot({
       autoSchemaFile: true,
     }),
