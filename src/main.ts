32bfb9e1e7b720d9a80e523fb8084aa7c936f454
SeungGumon <b9327912@gmail.com>
2021-10-28 23:17:59 +0900

    Update main.ts
    진짜 애플 싸대기마렵네 ;
    포트넘버 5001 로 변경

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(5000);
+  await app.listen(5001);
 
 }
 bootstrap();
