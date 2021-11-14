347bcb6b408d2faad50eca79ffdb1b127431b9a4
SeungGumon <b9327912@gmail.com>
2021-11-15 01:32:57 +0900

    change PORT

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
-  await app.listen(parseInt(process.env.PORT) || 3000);
+  await app.listen(parseInt(process.env.PORT) || 5001);
 
 }
 bootstrap();
