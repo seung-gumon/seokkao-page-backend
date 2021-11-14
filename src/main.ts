4e1e0ee693052a1cd973ef0d2e1ce5e65c342066
SeungGumon <b9327912@gmail.com>
2021-11-15 01:07:30 +0900

    console.log

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+  console.log(process.env.PORT , "PORT NUMBER")
   await app.listen(process.env.PORT || 5001);
 
 }
