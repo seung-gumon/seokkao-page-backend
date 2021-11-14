899249cc6c3308bd8bd664c8547905698e2a8a37
SeungGumon <b9327912@gmail.com>
2021-11-15 01:22:53 +0900

    main ts errro

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
-  await app.listen(process.env.PORT || 5001);
+  await app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
 
 }
 bootstrap();
