d56f0e1098a5c0f61c0a356c166bfe77a09181a8
SeungGumon <b9327912@gmail.com>
2021-11-15 02:00:15 +0900

    change app module and main.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -36,12 +36,15 @@ import { EpisodeModule } from './episode/episode.module';
         }),
         TypeOrmModule.forRoot({
             type: 'postgres',
-            host: process.env.DB_HOST,
-            port: +process.env.DB_PORT,
-            username: process.env.DB_USERNAME,
-            password: process.env.DB_PASSWORD,
-            database: process.env.DB_DATABASE,
-            synchronize: true,
+            ...(process.env.DATABASE_URL ? {url: process.env.DATABASE_URL}
+                : {
+                    host: process.env.DB_HOST,
+                    port: +process.env.DB_PORT,
+                    username: process.env.USERNAME,
+                    password: process.env.PASSWORD,
+                    database: process.env.DB_NAME,
+                }),
+            synchronize:process.env.NODE_ENV !== 'prod',
             logging: false,
             entities: [User, Category , Series , Part , Episode , PurChaseHistory],
         }),
