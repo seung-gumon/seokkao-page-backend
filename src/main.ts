92d705422612288c00328709de511a78b423acd6
SeungGumon <b9327912@gmail.com>
2021-11-15 01:14:21 +0900

    add mainTs use Global Pipes

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {ValidationPipe} from "@nestjs/common";
 
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
-  console.log(process.env.PORT , "PORT NUMBER")
   await app.listen(process.env.PORT || 5001);
 
 }
