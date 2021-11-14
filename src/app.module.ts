258caf9d1ed19bd5f1c0c8daa3c77fb0b459992a
SeungGumon <b9327912@gmail.com>
2021-11-15 02:27:25 +0900

    change ENVNAME

--- src/app.module.ts
+++ src/app.module.ts
@@ -40,7 +40,7 @@ import { EpisodeModule } from './episode/episode.module';
             port: +process.env.DB_PORT,
             username: process.env.DB_USERNAME,
             password: process.env.DB_PASSWORD,
-            database: process.env.DB_DATABASE,
+            database: process.env.DB_NAME,
             synchronize: true,
             logging: false,
             entities: [User, Category , Series , Part , Episode , PurChaseHistory],
