2e51e211415f1bd9af6341768e905172cb1802ce
SeungGumon <b9327912@gmail.com>
2021-11-15 01:24:46 +0900

    change main ts

--- src/main.ts
+++ src/main.ts
@@ -7,9 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
-  await app.listen(process.env.PORT || 3000, function(){
-    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-  });
+  await app.listen(parseInt(process.env.PORT) || 5001);
 
 }
 bootstrap();
