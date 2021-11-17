dd986563eb42957137119b0311176b3aafaa9a4e
SeungGumon <b9327912@gmail.com>
2021-11-17 12:14:54 +0900

    Dev 환경 그래프큐엘 포트넘버 5001로 변경

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import {ValidationPipe} from "@nestjs/common";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(process.env.PORT || 4000);
+  await app.listen(process.env.PORT || 5001);
 
 }
 bootstrap();
